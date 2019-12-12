207391f0489e2c4450a6cdd59c3f1e8283eefe21
Mac <mac@MacdeMacBook-Pro-3.local>
2019-12-13 02:49:54 +0800

    Site updated: 2019-12-13 02:49:53

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      }
+      
+    ]
+});
